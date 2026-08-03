7e318d7b4cee28624a261fc4ae2e8a7151b2f955
Efrain Gusman <joelviettro@gmail.com>
2026-08-02 21:40:02 -0300

    Torneo: permitir cargar goles/asistencias/tarjetas sin iniciar el cronometro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'canchero-v409';
+const CACHE_NAME = 'canchero-v410';
 const RUNTIME_CACHE = 'canchero-runtime'; // persistente entre deploys (media/JS/CSS cacheados)
 const assets = [
   './',
